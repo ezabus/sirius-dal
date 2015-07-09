276ee8e795ed3f51f7fc09be73160a6a3a2e03d5
ezabus <evgenzabus@gmail.com>
2015-07-09 20:18:03 +0300

    Create ClusterRepostiory.java

---
+++ src/main/java/org/zabus/sirius/dal/ClusterRepository.java
@@ -0,0 +1,9 @@
+package org.zabus.sirius.dal;
+
+import org.springframework.data.repository.CrudRepository;
+
+/**
+ * Created by user on 09.07.2015.
+ */
+public interface ClusterRepository extends CrudRepository {
+}
